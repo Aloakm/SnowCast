31ee3ef5234d748cec159b9d37e4deaffb0097f1
Aloak <aloak92@gmail.com>
2017-03-06 18:40:18 -0800

    added links to carousel

--- src/components/landing/carousel.js
+++ src/components/landing/carousel.js
@@ -6,14 +6,14 @@ export default class Carousel extends Component {
     return(
       <div style={{height:350,margin:0}}>
         <React_Boostrap_Carousel animation={true} className="carousel-fade">
-          <div style={{height:350,width:"100%",backgroundColor:"black",paddingTop:'100px'}}>
-            <div style={{top:'100px'}}>123</div>
+          <div style={{height:350,width:"100%",backgroundSize: "100%",backgroundImage: "url('http://img1.onthesnow.com/image/gg/48/1_485040.jpg')",paddingTop:'300px', paddingLeft:'70px'}}>
+            <div><a style={{color: 'black'}} href="http://www.onthesnow.com/gallery/p/photo/4374/jackson-hole-mountain-resort-2-17-id485042"><h3>Photo Gallery: Favorite Pics from February</h3></a></div>
           </div>
-          <div style={{height:350,width:"100%",backgroundColor:"blue"}}>
-            456
+          <div style={{height:350,width:"100%",backgroundSize: "100%",backgroundImage: "url('http://img3.onthesnow.com/image/gg/48/1_485042.jpg')",paddingTop:'300px', paddingLeft:'70px'}}>
+            <div><a style={{color: 'black'}} href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=newssearch&cd=1&cad=rja&uact=8&ved=0ahUKEwi-5Nv4rMPSAhXGi1QKHVlTA9MQqQIIGigAMAA&url=http%3A%2F%2Fmynorth.com%2F2016%2F12%2Fcrystal-mountain-liftopia-named-2-ski-resort%2F&usg=AFQjCNGybZP5NACUMAzI3B-ufvMXOF2Bsg&sig2=LBLZpLJbxfBP4jrQyfgD6g"><h3>Crystal Mountain Named #2 Ski Resort in US and Canada by Liftopia</h3></a></div>
           </div>
-          <div style={{height:350,width:"100%",backgroundColor:"green"}}>
-            789
+          <div style={{height:350,width:"100%",backgroundSize: "100%",backgroundImage: "url('http://img3.onthesnow.com/image/gg/48/1_485048.jpg')",paddingTop:'315px', paddingLeft:'70px'}}>
+            <div><a style={{color: 'black'}} href="https://www.youtube.com/watch?v=NWeWCw0HmLU"><h4>MUST WATCH: Crystal Mountain powder weekend</h4></a></div>
           </div>
         </React_Boostrap_Carousel>
       </div>
--- src/components/main/favorite-tabs.js
+++ src/components/main/favorite-tabs.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default (props) => {
   return(
-    <div className="row">
-      <div className="col-md-8 clicklink" onClick={()=>props.fetch(props.fav.nameString)}>{props.fav.nameString}</div>
-    </div>
+      <div className="clicklink" onClick={()=>props.fetch(props.fav.nameString)}>
+        {props.fav.nameString}
+      </div>
+
   )
 }
